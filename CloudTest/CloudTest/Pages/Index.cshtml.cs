c95e8b516b17d7bbad3e097421fa5bad55c315db
Sushil Pandey <Sushil.Pandey@crowe.com>
2019-04-06 09:11:19 +0530

    new project added

---
+++ CloudTest/CloudTest/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
